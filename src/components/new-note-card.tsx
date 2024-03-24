6343ccb2e589e20807cfd61bd0949cd9891a1648
Jessica Adamek <jessica.g.adamek@gmail.com>
2024-03-24 19:52:09 -0300

    :art: style note-card dialog

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,7 +3,7 @@ export function NewNoteCard() {
     <div className="rounded-md bg-slate-700 p-5 space-y-3">
       <span className="text-sm font-medium text-slate-200">Adicionar nota</span>
       <p className="text-sm leading-6 text-slate-400">
-        Gravee uma nota em áudio que será convertida para texto automaticamente.
+        Grave uma nota em áudio que será convertida para texto automaticamente.
       </p>
     </div>
   );
